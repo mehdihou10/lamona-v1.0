a92a5555a51bbfeebf6d06248aac8dd1e4e67924
mehdi hou <mehdi5juillet@gmail.com>
2024-10-17 12:21:47 +0200

    fix email

--- utils/send.email.js
+++ utils/send.email.js
@@ -1,30 +1,26 @@
 import nodemailer from 'nodemailer';
 
-export const sendEmail = (html,email,subject)=>{
-
+export const sendEmail = async (html, email, subject) => {
+  try {
+    
     const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.USER_EMAIL,
-            pass: process.env.USER_PASSWORD
-        }
-    })
+      service: "gmail",
+      auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.USER_PASSWORD,
+      },
+    });
 
     const mailOptions = {
+      from: process.env.USER_EMAIL,
+      to: email,
+      subject,
+      html,
+    };
 
-        from: process.env.USER_EMAIL,
-        to: email,
-        subject,
-        html
-    }
-
-      transporter.sendMail(mailOptions,(err,success)=>{
-
-        if(err){
-
-            console.log(err)
-
-        }
-      });
-
-}
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent successfully:", info.response);
+  } catch (err) {
+    console.log("Error sending email:", err);
+  }
+};
